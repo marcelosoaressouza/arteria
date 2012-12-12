0a9bc13490ef038238ff9cc00630f637af6694af
Marcelo Soares Souza <marcelo@juntadados.org>
2012-12-12 15:32:02 -0200

    * Minor fixes * Add Attachments to CkEditor in Posts * Added Arteria Logo

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -2,12 +2,12 @@ class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
                     :url  => "/pictures/:id/:style_:basename.:extension",
                     :path => ":rails_root/public/pictures/:id/:style_:basename.:extension",
-	                  :styles => { :content => '800>', :thumb => '118x100#' }
+                    :styles => { :content => '800>', :thumb => '118x100#' }
+
+  validates_attachment_size :data, :less_than => 8.megabytes
+  validates_attachment_presence :data
 	
-	validates_attachment_size :data, :less_than => 8.megabytes
-	validates_attachment_presence :data
-	
-	def url_content
-	  url(:content)
-	end
+  def url_content
+    url(:content)
+  end
 end
